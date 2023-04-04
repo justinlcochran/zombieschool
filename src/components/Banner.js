19ff741b0e613be22450b4a120c89bb2d20d5175
Justin <justinlcochran@gmail.com>
2023-04-03 21:06:44 -0600

    initialized home screen

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Banner(props) {
+    return (
+        <div>
+            <img className={"w-screen"} src="https://ucarecdn.com/9ab7f62c-4f86-40a9-9fdc-5d4f0ed37cf1/ZombieSchoolBG.png" alt="Zombie School" />
+        </div>
+    );
+}
+
+export default Banner;
