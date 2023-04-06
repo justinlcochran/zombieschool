5d2c4a6c8744cf3a673e6c726506365ddebccbc1
Justin <justinlcochran@gmail.com>
2023-04-05 20:02:14 -0600

    modal mockup complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 import Banner from "./components/Banner";
+import ActivityModal from "./components/ActivityModal";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App bg-black h-screen">
+    <div className="App bg-white h-screen">
       <BrowserRouter>
         <Routes>
-            <Route exact path='/' element={<Banner />} />
+            <Route exact path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
