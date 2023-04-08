658248a285a35981369dd03d926e767469baa30c
Justin <justinlcochran@gmail.com>
2023-04-08 12:37:11 -0600

    Time releasing is up

--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -1,42 +1,67 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { ReactComponent as CaretDown } from "../assets/caret-down-bold-svgrepo-com.svg";
 
-function Week({ week, index, setShowModal }) {
+function Week({ week, index, setShowModal, date }) {
   const [expanded, setExpanded] = useState(false);
 
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
+  const release = new Date('2023-03-28');
+  release.setDate(release.getDate() + (7 * index));
+  const dayDiff = Math.floor((release - date) / (1000 * 60 * 60 * 24));
+  const hourDiff = Math.floor(((release - date) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
   return (
-    <div>
-      <div className="m-auto h-auto w-11/12 mb-1">
-        <div
-          className="flex justify-between items-center px-4 bg-blue-600 hover:bg-blue-700 cursor-pointer p-4 rounded-t-2xl rounded-bl-2xl "
-          onClick={toggleExpanded}
-        >
-          <h1 className="leading-8 text-2xl font-medium">{`Week ${index}:`}</h1>
-          <CaretDown className={`h-8 w-8 transition ${expanded ? "-scale-y-100" : ""}`} />
-        </div>
-        <div
-          /* The transition requires starting and ending heights not be dynamic so we must set those heights via js.
-          Overflow used to hide items - we are not conditionally rendering the expanded items 
-          since the collapsing transition closes immediately when the items are not rendered. */
-          className={`overflow-hidden grid grid-cols-4 gap-2 p-2 min-h-full rounded-b-2xl transition-all duration-300 ease-in-out bg-blue-950 w-[90%] ml-auto mr-0
-          ${expanded ? "opacity-100" : "opacity-0"}
-          `}
-          // use style instead of tailwind since height is programmatically generated
-          style={{ height: expanded ? `${2 * week.activities.length}rem` : "0" }}
-        >
-          {week.activities.map((activity, index) => (
-            <div className={"p-4 bg-violet-700 rounded-xl hover:bg-violet-600 cursor-pointer select-none my-auto font-bold text-2xl"} onClick={() => setShowModal(activity)}>
-              {activity.title}
+      <>
+        { dayDiff > 0 && dayDiff < 8 ? (
+
+            <div>
+              <div className="m-auto h-auto w-11/12 mb-1">
+                <div
+                    className="flex justify-between items-center px-4 bg-gray-600 hover:bg-gray-700 cursor-pointer p-4 rounded-t-2xl rounded-bl-2xl "
+
+                >
+                  <h1 className="leading-8 text-2xl font-medium select-none">{`Week ${index}:`}</h1>
+                  <h1 className="leading-8 text-2xl font-medium select-none">{`Opens in ${dayDiff} days, ${hourDiff} hours`}</h1>
+                </div>
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+        ) : dayDiff <= 0 ? (
+            <div>
+              <div className="m-auto h-auto w-11/12 mb-1">
+                <div
+                    className="flex justify-between items-center px-4 bg-blue-600 hover:bg-blue-700 cursor-pointer p-4 rounded-t-2xl rounded-bl-2xl "
+                    onClick={toggleExpanded}
+                >
+                  <h1 className="leading-8 text-2xl font-medium">{`Week ${index}:`}</h1>
+                  <CaretDown className={`h-8 w-8 transition ${expanded ? "-scale-y-100" : ""}`}/>
+                </div>
+                <div
+                    /* The transition requires starting and ending heights not be dynamic so we must set those heights via js.
+                    Overflow used to hide items - we are not conditionally rendering the expanded items
+                    since the collapsing transition closes immediately when the items are not rendered. */
+                    className={`overflow-hidden grid grid-cols-4 gap-2 p-2 min-h-full rounded-b-2xl transition-all duration-300 ease-in-out bg-blue-950 w-[90%] ml-auto mr-0
+      ${expanded ? "opacity-100" : "opacity-0"}
+      `}
+                    // use style instead of tailwind since height is programmatically generated
+                    style={{height: expanded ? `${2 * week.activities.length}rem` : "0"}}
+                >
+                  {week.activities.map((activity, index) => (
+                      <div
+                          className={"p-4 bg-violet-700 rounded-xl hover:bg-violet-600 cursor-pointer select-none my-auto font-bold text-2xl"}
+                          onClick={() => setShowModal(activity)}>
+                        {activity.title}
+                      </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+        ) : (
+            <></>
+        )}
+      </>
+  )
 }
 
 export default Week;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import ActivityModal from "../components/ActivityModal";
 import Week from "../components/Week";
 import weeks from "../assets/weeks.json";
 
 function Home() {
-  const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [currentDate, setCurrentDate] = useState(false)
 
-  return (
-    <div>
-      {weeks.map((week, index) => (
-        <Week week={week} index={index + 1} setShowModal={setShowModal} />
-      ))}
-      <ActivityModal modalContent={showModal} onClose={() => setShowModal(false)} />
-    </div>
-  );
+    useEffect(() => {
+        setCurrentDate(new Date());
+    }, []);
+
+
+    return (
+        <div>
+            {weeks.map((week, index) => (
+                <Week week={week} index={index + 1} date={currentDate} setShowModal={setShowModal}/>
+            ))}
+            <ActivityModal modalContent={showModal} onClose={() => setShowModal(false)}/>
+        </div>
+    );
 }
 
 export default Home;
