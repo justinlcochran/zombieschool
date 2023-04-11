fb4a4e87c9c4f21892688713561ea68bfbb25c08
Justin <justinlcochran@gmail.com>
2023-04-10 21:19:40 -0600

    BG remade for mobile, kept desktop bg

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,15 @@ import React from "react";
 function Banner({ scroll }) {
   const blur = Math.min(scroll / 30, 5);
 
+    const getBG = () => {
+        const viewportWidth = window.innerWidth;
+        if (viewportWidth < 640) {
+            return "https://ucarecdn.com/8cf1461b-89de-47f5-8af2-a9f260289428/ZombieSchool1.png";
+        } else {
+            return "https://ucarecdn.com/9ab7f62c-4f86-40a9-9fdc-5d4f0ed37cf1/ZombieSchoolBG.png";
+        }
+    };
+
   return (
     <>
       {/* black bg avoid white halo when blurring */}
@@ -10,7 +19,7 @@ function Banner({ scroll }) {
       <img
         className={"fixed h-screen w-screen  -z-10"}
         style={{ filter: `blur(${blur}px)` }}
-        src="https://ucarecdn.com/9ab7f62c-4f86-40a9-9fdc-5d4f0ed37cf1/ZombieSchoolBG.png"
+        src={getBG()}
         alt="Zombie School"
       />
     </>
